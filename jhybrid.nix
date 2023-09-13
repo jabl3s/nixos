97421ad4ae8e2cfbaacf62680158a3a3705b1956
J <j@jabl3s.uk>
2023-09-13 23:29:35 +0100

    Create jhybrid.nix

---
+++ jhybrid.nix
@@ -0,0 +1,36 @@
+{#I believe nix supports comments, but can I specify my own hidden env vars file for tools to use in this nix template  
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-Samsung_SSD_850_EVO_250GB_S21PNXAGB12345";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
