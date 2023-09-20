102efb4e1972d6269b50f8d61d93693836f2bc49
J <j@jabl3s.uk>
2023-09-20 05:08:06 +0100

    hyperland syntax

--- jtower-gen-12/configuration.nix
+++ jtower-gen-12/configuration.nix
@@ -50,12 +50,12 @@
       enable = true;
       nvidiaPatches = true;
       xwayland.enable = true;
-    }
+    };
   };
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
-  }
+  };
   xdg.portal.enable=true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   services = {
