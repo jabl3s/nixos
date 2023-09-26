6722a672d12e852af1169d4aee43a00bbdfa6f4f
J <j@jabl3s.uk>
2023-09-26 14:29:46 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,15 +1,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-
-  # Bootloader.
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
@@ -30,26 +26,18 @@
     blacklistedKernelModules = [ "nouveau" "i915" ];
     kernelParams = [ "quiet" "nomodeset" ];
   };
-  # Networking
+  security.rtkit.enable = true;
+  console = { earlySetup = true; keyMap = "uk"; };
   networking.extraHosts = let hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts;
   hostsFile = builtins.fetchurl hostsPath;
   in builtins.readFile "${hostsFile}";
   networking.hostName = "jtower";
   networking.networkmanager.enable = true;
-  #networking.wireless.iwd.enable = true;
-  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  # Set your time zone.
   time.timeZone = "Europe/London";
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
   environment.sessionVariables = {
-    #WLR_NO_HARDWARE_CURSORS = "1"; uncomment for sddm into sawy
     NIXOS_OZONE_WL = "1"; #Discord in wayland
   };
-  # Hardware
   sound.enable = true;
   hardware = {
     enableRedistributableFirmware = true;
@@ -64,11 +52,10 @@
     };
     nvidia             = {
       modesetting.enable = true;
-      forceFullCompositionPipeline = true; 
-      # => pop os has forceCompositionPipeline no "full"
+      forceFullCompositionPipeline = true; #=>pop os has forceCompositionPipeline no "full"
       nvidiaSettings  = true;
       powerManagement = {
-        enable      = false;
+        enable      = true;
         finegrained = false;
       };
       open = true;
@@ -76,7 +63,7 @@
     };
     pulseaudio.enable = false;
   };
-  # Enable the X11 windowing system.
+  services.openssh.enable = true;
   services.xserver = {
     enable               = true;
     videoDrivers = [ "nvidia" ];
@@ -87,80 +74,34 @@
       autoLogin  = { enable = true; user = "j"; };
       gdm.enable = true;
     };
-  };
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-  
-  # Configure console keymap
-  console = {
-    earlySetup = true;
-    keyMap = "uk";
-  };
-
-  # Enable CUPS to print documents.
+  };  
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.j = {
+  users.users.j = { # Define a user account. Don't forget to set a password with ‘passwd’ from nixos-enter.
     isNormalUser = true;
     description = "j";
     extraGroups = [ "networkmanager" "wheel" ];
   };
-
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  programs.steam.enable = true;
   environment.systemPackages = with pkgs; [
-    tmux
-    sshpass
+  # Base
+    networkmanager iwd libnotify xwayland wayland
+    pciutils usbutils wget file unzip 
+    wl-clipboard wol wmctrl solaar
+  # Development
+    tmux sshpass git
     (python3.withPackages(ps: with ps; [       
-      tk
-      tkinter
-      pandas
-      requests
-      numpy
-      pendulum
-      pillow
-      moviepy
-      pyqt5
-      pyqt6
-      pytest
-      briefcase
+      tk tkinter pandas requests numpy
+      pendulum pillow moviepy pyqt5 pyqt6
+      pytest #briefcase
       ]))
-    curl
-    networkmanager
-    iwd
-    libnotify
-    xwayland
-    wayland
-    lutris
-    pciutils
-    usbutils
-    firefox-wayland
-    vscode
-    (vscode-with-extensions.override {
+     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
       bbenoist.nix
       ms-python.python
@@ -172,54 +113,22 @@
         version = "0.47.2";
         sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
       }];})
-    kate
-    file 
-    unzip 
-    wl-clipboard 
-    wol
-    wmctrl
-    wget  
-    discord
-    solaar 
-    wineWowPackages.staging
-    wineWowPackages.waylandFull
+    ## Beeware dependencies
+    build-essential pkg-config python3-dev python3-venv libgirepository1.0-dev libcairo2-dev gir1.2-webkit2-4.0 libcanberra-gtk3-module
+  # Gaming
+    lutris firefox-wayland discord wineWowPackages.staging wineWowPackages.waylandFull
     winetricks
-    git
-    #beeware dependencies
-    build-essential pkg-config python3-dev python3-venv libgirepository1.0-dev 
-    libcairo2-dev gir1.2-webkit2-4.0 libcanberra-gtk3-module
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
    programs.mtr.enable = true;
    programs.gnupg.agent = {
      enable = true;
      enableSSHSupport = true;
    };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
-  powerManagement.cpuFreqGovernor = "ondemand";
   systemd.services = {
     # https://github.com/NixOS/nixpkgs/issues/103746
     "getty@tty1".enable  = false;
     "autovt@tty1".enable = false;
   };
+  powerManagement.cpuFreqGovernor = "ondemand";
 }
