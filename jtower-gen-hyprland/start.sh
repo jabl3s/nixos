df374cb470f306980f831f4435e2bb547b9d5a39
J <j@jabl3s.uk>
2023-09-28 09:53:01 +0100

    Update start.sh

--- jtower-gen-hyprland/start.sh
+++ jtower-gen-hyprland/start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+/nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}')/libexec/polkit-kde-authentication-agent-1 &  
 swww init &
 swww img /home/j/Wallpapers/desktop.jpeg &
 nm-applet --indicator &
 waybar &
-#steam &
-#firefox --new-window https://youtube.com &
+steam &
+firefox --new-window https://youtube.com &
 firefox --new-window https://github.com &
 code ~/Code &
 code /etc/nixos &
