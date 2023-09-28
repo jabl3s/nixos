971c1007c81f17bc23466dde4160aabcb81cc140
J <j@jabl3s.uk>
2023-09-28 03:06:27 +0100

    Create start.sh

---
+++ jtower-gen-hyprland/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+swww init &
+swww img /home/j/Wallpapers/desktop.jpeg &
+nm-applet --indicator &
+waybar &
+#steam &
+#firefox --new-window https://youtube.com &
+firefox --new-window https://github.com &
+code ~/Code &
+code /etc/nixos &
+code /home/j/.config/hypr &
+#konsole &
