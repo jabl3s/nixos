13b9d0b7bc07f34932bf2527d6300b5d41214e1a
J <j@junder.uk>
2024-07-08 02:58:36 +0100

    Update start.sh

--- jtower-gen-hyprland/start.sh
+++ jtower-gen-hyprland/start.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-/nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}')/libexec/polkit-kde-authentication-agent-1 &  
+#/nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}')/libexec/polkit-kde-authentication-agent-1 &  
 swww init &
 swww img /home/j/Wallpapers/desktop.jpeg &
 nm-applet --indicator &
 waybar &
-steam &
-firefox --new-window https://youtube.com &
-firefox --new-window https://github.com &
-code ~/Code &
-code /etc/nixos &
-code /home/j/.config/hypr &
+#steam &
+#firefox --new-window https://youtube.com &
+#firefox --new-window https://github.com &
+#code ~/Code &
+#code /etc/nixos &
+#code /home/j/.config/hypr &
 #konsole &
