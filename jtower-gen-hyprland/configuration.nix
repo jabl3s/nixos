7e8b0ceb0ce147f7dc613d822a26d604f25fe337
J <j@junder.uk>
2024-07-16 02:20:48 +0100

    Update configuration.nix

--- jtower-gen-hyprland/configuration.nix
+++ jtower-gen-hyprland/configuration.nix
@@ -1,115 +1,121 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-    # Bootloader.
-    boot = {
-      loader = {
-        efi.canTouchEfiVariables = true;
-        systemd-boot = {
-          enable = true;
-          configurationLimit = 10;
-        };
-      };
-    plymouth = {
-      enable = true;
-      extraConfig = "DeviceScale=1";
-    };
-    initrd = {
-      kernelModules = [ "nvme" "ahci" ];
-      network.enable = true;
-      systemd.enable   = true;
-    };
-    blacklistedKernelModules = [ "nouveau" "i915" ];
-    kernelParams = [ "quiet" "nomodeset" ];
-  };
-  systemd.services = {
-    # https://github.com/NixOS/nixpkgs/issues/103746
-    "getty@tty1".enable  = false;
-    "autovt@tty1".enable = false;
-  };
-  # Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based configurations
-  sound.enable=false;
-  # rtkit is optional but recommended
-  security.rtkit.enable = true;
-  console = { earlySetup = true; keyMap = "uk"; };
-  networking.extraHosts = let hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts;
-  hostsFile = builtins.fetchurl hostsPath;
-  in builtins.readFile "${hostsFile}";
-  networking.hostName = "jtower";
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "JTOWER"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-  hardware = {
-    enableRedistributableFirmware = true;
-    bluetooth.enable   = false;
-    cpu.intel          = { updateMicrocode = true; };
-    keyboard.uhk       = { enable = true; };
-    opengl             = {
-      driSupport      = true;
-      driSupport32Bit = true;
-      enable          = true;
-      extraPackages   = with pkgs; [ libvdpau-va-gl vaapiIntel vaapiVdpau nvidia-vaapi-driver intel-media-driver ];
-    };
-    nvidia             = {
-      modesetting.enable = true;
-      forceFullCompositionPipeline = true; #=>pop os has forceCompositionPipeline no "full"
-      nvidiaSettings  = true;
-      powerManagement = {
-        enable      = false;
-        finegrained = false;
-      };
-      open = false;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-    pulseaudio.enable = false;
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
-  services.openssh.enable = false;
-  services.printing.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "gb";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.j = {
+    isNormalUser = true;
+    description = "j";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "j";
+  services.greetd = {
+  enable = true;
+  settings = rec {
+    initial_session = {
+      command = "Hyprland";
+      user = "j";
+    };
+    default_session = initial_session;
+  };
+};
+
+# Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based >
+security.rtkit.enable = true;  
+sound.enable=false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-  services.xserver = { # xorg
-    enable               = true;
-    videoDrivers = [ "nvidia" ];
-    layout = "gb";
-    xkbVariant = "";
-    displayManager       = {
-      autoLogin  = { enable = true; user = "j"; };
-      gdm.enable = true;
-    };
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+  context.properties = {
+    default.clock.rate = 48000;
+    default.clock.quantum = 32;
+    default.clock.min-quantum = 32;
+    default.clock.max-quantum = 32;
   };
-  services.gvfs.enable = true ;
-  services.tumbler.enable = true;  
-  ##services.xserver.displayManager.defaultSession = "plasmawayland";
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
+};
+services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+  context.modules = [
+    {
+      name = "libpipewire-module-protocol-pulse";
+      args = {
+        pulse.min.req = "32/48000";
+        pulse.default.req = "32/48000";
+        pulse.max.req = "32/48000";
+        pulse.min.quantum = "32/48000";
+        pulse.max.quantum = "32/48000";
+      };
+    }
+  ];
+  stream.properties = {
+    node.latency = "32/48000";
+    resample.quality = 1;
   };
-  xdg.portal.enable=true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  programs = {
-    thunar.enable = true;
-    nix-ld.enable = true; # https://unix.stackexchange.com/a/522823
-    steam.enable = true;
-    hyprland = {
-      enable = true;
-      xwayland.enable = true; # x translation layer
-    };
-  };
-  users.users.j = { # Define a user account. Don't forget to set a password with ‘passwd’ from nixos-enter.
-    isNormalUser = true;
-    description = "j";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-  nixpkgs.config.allowUnfree = true;  
+};
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
   # Base
     networkmanager networkmanagerapplet iwd libnotify xwayland wayland
     pciutils usbutils wget file unzip gimp alsa-utils polkit-kde-agent sshguard 
@@ -120,11 +126,84 @@
     lutris firefox-wayland discord wineWowPackages.staging wineWowPackages.waylandFull
     winetricks
   ];
-   programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
+  
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = false;
+  hardware.nvidia.powerManagement.finegrained = false;
+  hardware.nvidia.open = false;
+  hardware.nvidia.nvidiaSettings = true;
+  # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
+  hardware.nvidia.package = let 
+  rcu_patch = pkgs.fetchpatch {
+    url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02->
+    hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+  };
+  in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "535.154.05";
+    sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+    sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+    openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+    settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+    persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+
+    #version = "550.40.07";
+    #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
+    #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
+    #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
+    #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
+    #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
+
+    patches = [ rcu_patch ];
+  };
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+  programs.thunar.enable = true;
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};  
+  
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-  powerManagement.cpuFreqGovernor = "ondemand";
+
 }
+
+
